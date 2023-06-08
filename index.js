fa76157830787ff52ebc7e58f10a33c2f5989cdd
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-06-08 13:49:08 +0000

    Fix linter

--- index.js
+++ index.js
@@ -14,18 +14,16 @@ class AwesomeBook {
 
     let listOfBooks = '';
     this.books.forEach((book, index) => {
-      listOfBooks += `<div >
+      listOfBooks +=`<div >
   <span class='name-of-book'>${book.title}</span>${'&nbsp;'.repeat(10)}
   <span class='author-of-book'>${book.author}</span>${'&nbsp;'.repeat(10)}
   <button type="button" class="remove-button" id="${index}">Remove</button>
   <hr>
 </div>`;
-
-    });
+  });
     this.bookList.innerHTML = listOfBooks;
 
     this.addRemoveEventListeners();
-  }
 
   clearFields =() => {
     const title = document.getElementById('bookname');
